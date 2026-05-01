b7244cbb187d4d4b2dd47d2141668b1eb78aff36
aliyanadaa <aliyanadaq@gmail.com>
2026-05-01 17:00:04 +0700

    menambahkan fungsi deleteNode (setup awal)

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -66,3 +66,18 @@ if (current->next != NULL)
 
 current->next = newnode;
 }
+
+void hapus()
+{
+    if (START == NULL)
+    {
+        cout << "\nList is empty" << endl; 
+        return;
+    }
+
+    cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+    int rollNo;
+    cin >> rollNo;
+
+   
+}
